2b1a8ee296651e53e00bd3f42fdde0ea5fed1ee3
Your Name <you@example.com>
2024-08-08 12:23:38 +0000

    feat: [OCR Patch]七律·雷电[毛泽东全集第52卷

---
+++ [0839f039e4][maoquanji52].ts
@@ -0,0 +1,4 @@
+
+export default [
+  {"version":2,"parts":{"0":{"insertAfter":[{"type":"paragraph","text":"积云忽作闷雷声，"},{"type":"paragraph","text":"雨弹光刀欲杀人。"},{"type":"paragraph","text":"岂向瑶台追幻梦，"},{"type":"paragraph","text":"还从烈火识真金。"},{"type":"paragraph","text":"几枝玉叶化灰蝶，"},{"type":"paragraph","text":"再度铁梅成赤心。"},{"type":"paragraph","text":"稳似泰山同携手，"},{"type":"paragraph","text":"陶然一笑友情深。"}],"diff":"=5\t-64\t+%EF%BC%8A","type":"title"}},"comments":{},"description":""},
+];
