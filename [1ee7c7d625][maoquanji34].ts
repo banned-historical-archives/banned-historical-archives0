5d228597aa21e4134192d56448536043dcbce735
Your Name <you@example.com>
2025-09-21 10:30:41 +0000

    feat: [OCR Patch]原子弹吓不倒中国人民[毛泽东全集第34卷

---
+++ [1ee7c7d625][maoquanji34].ts
@@ -0,0 +1,4 @@
+
+export default [
+  {"version":2,"parts":{"0":{"type":"title","diff":"=11\t+%E3%80%941%E3%80%95"},"1":{"type":"subdate"},"4":{"diff":"=230\t+%E4%BA%86%E4%BB%80%E4%B9%88%E3%80%82"},"5":{"diff":"=3\t-31","delete":true},"6":{"diff":"=10\t-4","delete":true}},"comments":{},"description":"","newComments":["这是毛泽东在芬兰首任驻中国大使孙士教递交国书的时候谈话的要点。"]},
+];
