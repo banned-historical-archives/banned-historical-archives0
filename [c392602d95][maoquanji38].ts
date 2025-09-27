70e88c7512d788825631008073e04920b999ca83
Your Name <you@example.com>
2025-09-27 04:52:17 +0000

    feat: [OCR Patch]为查找《反对哲学思想中的修正主义》一文写的批语[毛泽东全集第38卷

---
+++ [c392602d95][maoquanji38].ts
@@ -0,0 +1,4 @@
+
+export default [
+  {"version":2,"parts":{"0":{"diff":"=23\t-1\t+%E3%80%941%E3%80%95","type":"title"},"1":{"insertAfter":[{"type":"appellation","text":"林克〔2〕"}],"diff":"=12\t-3","type":"subdate"},"3":{"type":"signature"},"4":{"type":"signature"},"5":{"diff":"=3\t-110","delete":true},"6":{"diff":"=3\t-15","delete":true}},"comments":{},"description":"","newComments":["这个批语写在1957年9月15日《杭州日报》刊登的反右派论文摘要《哲学思想中有哪些修正主义观点》旁边。这篇摘要摘自中共中央高级党校哲学教研室副主任孙定国《反对哲学思想中的修正主义》一文，原载《哲学研究》1957年第四期。","林克，当时任毛泽东办公室秘书。"]},
+];
