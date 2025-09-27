7ec699f621cf625e5e2cf676e883d1786625f7d3
Your Name <you@example.com>
2025-09-27 04:57:58 +0000

    feat: [OCR Patch]对江青来信的批语[毛泽东全集第52卷

---
+++ [077393d1c4][maoquanji52].ts
@@ -0,0 +1,4 @@
+
+export default [
+  {"version":2,"parts":{"0":{"diff":"=8\t-1\t+%E3%80%941%E3%80%95"},"1":{"insertAfter":[{"type":"paragraph","text":"我近日有些不适，以不见为好。大局是好的。"}],"diff":"=13\t-20","type":"subdate"},"2":{"diff":"=6\t-30","type":"signature"}},"comments":{},"description":"","newComments":["这个批语写在江青1975年12月27日给毛泽东的信上。"]},
+];
